895ace644ab11ec4d2db07f82898022491a640f4
Slopy656 <mariusvoluta34@gmail.com>
2026-09-13 21:11:12 +0300

    fixed is valid

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -1,22 +1,23 @@
 #include "Coordinate.h"
 
-Coordinate::Coordinate(char l, int n){
-    letter=l;
-    number=n;
+Coordinate::Coordinate(char l, int n)
+{
+    letter = l;
+    number = n;
 }
-char Coordinate::getLetter() {
+char Coordinate::getLetter()
+{
     return letter;
 }
-int Coordinate::getNumber() {
+int Coordinate::getNumber()
+{
     return number;
 }
-int Coordinate::getCollumn() {
-    return letter-'a';
+int Coordinate::getCollumn()
+{
+    return letter - 'a';
 }
-int Coordinate::getRow() {
-    return 8-number;
-}
-
-bool Coordinate::isValid() {
-    return letter>='a' && letter <= 'h' && number>=1 && number <=8
+int Coordinate::getRow()
+{
+    return 8 - number;
 }
